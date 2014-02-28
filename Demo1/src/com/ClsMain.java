188b778d0cdc00c5cc18e1cd74516977526935aa
Mritunjay Kumar <mritunjayemail@gmail.com>
2014-02-28 21:56:02 +0530

    This is demo

--- Demo1/src/com/ClsMain.java
+++ Demo1/src/com/ClsMain.java
@@ -7,7 +7,7 @@ public class ClsMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My Name is Kahnasdsf");
+		System.out.println("My Name is My Name Is Khan");
 
 	}
 
