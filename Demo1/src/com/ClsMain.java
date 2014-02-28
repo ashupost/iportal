a19bc78c18db6f87ecf3bb961c77723bd925d992
Mritunjay Kumar <mritunjayemail@gmail.com>
2014-02-28 21:42:09 +0530

    First time Commmit

---
+++ Demo1/src/com/ClsMain.java
@@ -0,0 +1,14 @@
+package com;
+
+public class ClsMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Name is Kahna");
+
+	}
+
+}
